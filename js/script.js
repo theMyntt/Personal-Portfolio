86b967eaf54d7fc80096b41aef97bf45129ae433
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2025-01-01 18:42:40 -0300

    feat: cv and social media section

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$("#github").on("click", () => {
+    location.href = "https://github.com/theMyntt";
+    return;
+})
+
+$("#linkedin").on("click", () => {
+    location.href = "https://linkedin.com/in/garaújo";
+    return;
+})
+
+$("#cv").on("click", () => {
+    window.open("/public/cv.pdf", "_blank")
+})
